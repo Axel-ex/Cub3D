2f12dda3141b95aaa3021b5b2d8ec94ab02854b0
Axel Chabrerie <achabrer@c1r13s4.42porto.com>
2024-03-22 15:20:48 +0000

    compile on linux, minimap solved
    minimap needs to be NULL terminated

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: Axel <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 13:30:10 by Axel              #+#    #+#             */
-/*   Updated: 2024/03/21 08:30:29 by Axel             ###   ########.fr       */
+/*   Updated: 2024/03/22 13:52:39 by achabrer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ int	main(int argc, char *argv[])
 	
 	init_game();
 	render_minimap();
+	// render_square(&game()->screen_buff, (t_square){50, 50, 50, RED});
+	// mlx_put_image_to_window(game()->mlx, game()->mlx_win, game()->screen_buff.img, 0, 0);
 	mlx_loop(game()->mlx);
 	exit_game();
 	return (EXIT_SUCCESS);
