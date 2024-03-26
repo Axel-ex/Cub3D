7b675eb17371257c5a4d9e22c7fbce89d7c40c24
Axel <Axel@MacBook-Air.Home>
2024-03-26 09:45:57 +0000

    update

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: Axel <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 13:30:10 by Axel              #+#    #+#             */
-/*   Updated: 2024/03/21 08:30:29 by Axel             ###   ########.fr       */
+/*   Updated: 2024/03/22 13:42:24 by Axel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,9 @@ int	main(int argc, char *argv[])
 	(void)argc;
 	(void)argv;
 	init_game();
-	render_minimap();
+	// render_minimap();
+	render_square(&game()->screen_buff, (t_square){50, 50, 50, RED});
+	mlx_put_image_to_window(game()->mlx, game()->mlx_win, game()->screen_buff.img, 0, 0);
 	mlx_loop(game()->mlx);
 	exit_game();
 	return (EXIT_SUCCESS);
