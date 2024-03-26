6c2f231a71f66e8460c249cb9708eb5abd6ff08b
Axel <Axel@MacBook-Air.Home>
2024-03-26 09:52:14 +0000

    update

--- sources/main.c
+++ sources/main.c
@@ -6,11 +6,7 @@
 /*   By: Axel <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 13:30:10 by Axel              #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/03/22 13:42:24 by Axel             ###   ########.fr       */
-=======
-/*   Updated: 2024/03/22 13:52:39 by achabrer         ###   ########.fr       */
->>>>>>> 2f12dda3141b95aaa3021b5b2d8ec94ab02854b0
+/*   Updated: 2024/03/26 09:52:03 by Axel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +25,7 @@ int	main(int argc, char *argv[])
 	(void)argv;
 	
 	init_game();
-<<<<<<< HEAD
-	// render_minimap();
-	render_square(&game()->screen_buff, (t_square){50, 50, 50, RED});
-	mlx_put_image_to_window(game()->mlx, game()->mlx_win, game()->screen_buff.img, 0, 0);
-=======
 	render_minimap();
-	// render_square(&game()->screen_buff, (t_square){50, 50, 50, RED});
-	// mlx_put_image_to_window(game()->mlx, game()->mlx_win, game()->screen_buff.img, 0, 0);
->>>>>>> 2f12dda3141b95aaa3021b5b2d8ec94ab02854b0
 	mlx_loop(game()->mlx);
 	exit_game();
 	return (EXIT_SUCCESS);
